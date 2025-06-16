798dfd815b57fe324b209c776e83854130510fa9
NanjeofKro <77793340+NanjeofKro@users.noreply.github.com>
2025-06-16 16:54:03 +0200

    Update main.py

--- deglitching/main.py
+++ deglitching/main.py
@@ -273,7 +273,7 @@ def deglitch(energy, mu, group, e_window='xas', sg_window_length=9, sg_polyorder
     
     e2         = np.delete(ener, out1) #removes points that are poorly fitted by the S-G filter
     n2         = np.delete(mu_copy, out1)
-    f          = CubicSpline(e2, n2, kind='cubic') 
+    f          = CubicSpline(e2, n2) 
     interp_pts = f(energy[out1]) #interpolates for normalized mu at the removed energies
     
     for i, point in enumerate(out1):
